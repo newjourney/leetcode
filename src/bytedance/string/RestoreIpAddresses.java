9cccbdbddd519e3d1ea5f5d36b6250d2f1347a60
zhangxingkai <zhangxingkai@hoolai.com>
2018-12-06 15:47:22 +0800

    string problems

--- src/bytedance/string/RestoreIpAddresses.java
+++ src/bytedance/string/RestoreIpAddresses.java
@@ -1,5 +1,6 @@
 package bytedance.string;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,12 +11,33 @@ import java.util.List;
  * 输入: "25525511135" 
  * 输出: ["255.255.11.135", "255.255.111.35"]
  * 
+ * 
+ * TODO 还没理解
+ * 
  * @author xingkai.zhang
  */
 public class RestoreIpAddresses {
 
-    public List<String> method1(String s) {
-        // TODO
-        return null;
+    public static List<String> method1(String s) {
+        List<String> solutions = new ArrayList<String>();
+        restoreIp(s, solutions, 0, "", 0);
+        return solutions;
     }
+
+    private static void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+        if (count > 4) return;
+        if (count == 4 && idx == ip.length()) solutions.add(restored);
+        
+        for (int i=1; i<4; i++) {
+            if (idx+i > ip.length()) break;
+            String s = ip.substring(idx,idx+i);
+            if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
+            restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(method1("25525511135"));
+    }
+    
 }
